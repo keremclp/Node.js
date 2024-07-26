2c88ad8d5faef1625182bea4c1944a20a8989f8f
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-07-26 15:19:49 +0300

    add: all rutes

--- 3-Task-manager-api/controllers/tasks.js
+++ 3-Task-manager-api/controllers/tasks.js
@@ -1,7 +1,30 @@
 
 
 const getAllTasks = (req,res) =>{
-    res.send('all items')
+    res.send('get all items')
 }
 
-module.exports = getAllTasks
+const createTask = (req,res) =>{
+    res.send('create item')
+
+}
+const getTask = (req,res) =>{
+    res.send('get single item')
+
+}
+const updateTask = (req,res) =>{
+    res.send('update item')
+
+}
+const deleteTask = (req,res) =>{
+    res.send('delete item')
+
+}
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}
--- 3-Task-manager-api/routes/tasks.js
+++ 3-Task-manager-api/routes/tasks.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const getAllTasks = require('../controllers/tasks')
+const {getAllTasks,createTask,deleteTask,getTask,updateTask} = require('../controllers/tasks')
 
-router.route('/').get(getAllTasks)
+router.route("/").get(getAllTasks).post(createTask);
+router.route('/:id').delete(deleteTask)
+router.route('/:id').get(getTask)
+router.route('/:id').put(updateTask)
 
 module.exports = router;
