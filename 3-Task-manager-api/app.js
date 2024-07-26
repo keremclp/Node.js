5660fa1fd7c8c59b4e9f5bb8f31ffee00b563dbe
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-07-26 15:11:45 +0300

    add: getAllTasks

--- 3-Task-manager-api/app.js
+++ 3-Task-manager-api/app.js
@@ -2,10 +2,15 @@ console.log('Task Manager App')
 
 const express = require('express')
 const app = express()
+const tasks = require('./routes/tasks')
+// middlewear
+app.use(express.json())
 
 app.get('/home', (req,res)=>{
     res.send('Home page')
 })
+
+app.use('/api/v1/tasks', tasks)
 
 const port = 3000
 
