d5736bc2a9639444749739f05c8f77919c71d841
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-07-26 23:28:51 +0300

    add: createTask func

--- 3-Task-manager-api/controllers/tasks.js
+++ 3-Task-manager-api/controllers/tasks.js
@@ -1,12 +1,13 @@
-const { raw } = require("express")
+const Task = require('../models/Task')
 
 
 const getAllTasks = (req,res) =>{
     res.send('get all items')
 }
 
-const createTask = (req,res) =>{
-    res.json(req.body)
+const createTask = async (req,res) =>{
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
 
 }
 const getTask = (req,res) =>{
