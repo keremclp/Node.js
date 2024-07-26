c17cabf3c352365654076ece503222fcaf4cf48b
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-07-26 14:45:12 +0300

    add: setup  basic express server

--- 3-Task-manager-api/app.js
+++ 3-Task-manager-api/app.js
@@ -1,1 +1,12 @@
 console.log('Task Manager App')
+
+const express = require('express')
+const app = express()
+
+app.get('/home', (req,res)=>{
+    res.send('Home page')
+})
+
+const port = 3000
+
+app.listen(port, console.log(`Server is listening on port ${port}...`));
