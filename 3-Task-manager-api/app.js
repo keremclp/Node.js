e5ac42c2c3e54c230e75a8ac73fa2a5dcbf76a06
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-07-26 22:40:47 +0300

    add: .env operations

--- 3-Task-manager-api/app.js
+++ 3-Task-manager-api/app.js
@@ -1,8 +1,9 @@
 console.log('Task Manager App')
-require('./db/connect')
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
 // middlewear
 app.use(express.json())
 
@@ -14,4 +15,13 @@ app.use('/api/v1/tasks', tasks)
 
 const port = 3000
 
-app.listen(port, console.log(`Server is listening on port ${port}...`));
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
--- 3-Task-manager-api/db/connect.js
+++ 3-Task-manager-api/db/connect.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://keremcan:kerem56425601.@cluster0.liabpdm.mongodb.net/task-manager-api?retryWrites=true&w=majority&appName=Cluster0";
+const connectDB = (url)=>{
+    return mongoose.connect(url)
+}
 
-mongoose
-  .connect(connectionString)
-  .then(() => console.log("connect to the database...."))
-  .catch((err) => console.log(err));
+module.exports = connectDB
