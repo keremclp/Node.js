4ceb6d7a312bb269847842d089dfa09d8b70a19b
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-07-27 14:16:58 +0300

    add: basic validation

--- 3-Task-manager-api/models/Task.js
+++ 3-Task-manager-api/models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose')
 
 const TaskShema = new mongoose.Schema({
-    name: String,
-    completed: Boolean,
+    name: {
+        type: String,
+        required : [true, 'must provide a name'],
+        trim: true,
+        maxlength: [20, 'name should 20 charachter']
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+    },
 })
 
 module.exports = mongoose.model('Task', TaskShema)
