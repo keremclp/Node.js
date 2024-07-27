eba74c7a3bb7dabdd782cd1e628528b8f41bd9e6
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-07-27 21:05:49 +0300

    add: notfound

--- 3-Task-manager-api/app.js
+++ 3-Task-manager-api/app.js
@@ -4,15 +4,17 @@ const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
-
+const notFound = require('./middlewear/not-found')
 // middlewear
 app.use(express.static('./public'))
 app.use(express.json())
 
+
+
 // routes
-
-
 app.use('/api/v1/tasks', tasks)
+
+app.use(notFound)
 
 const port = 3000
 
