38f1c7524f61269df676cb831fda582c01728760
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-07-28 00:02:24 +0300

    add: Custom Error

--- 3-Task-manager-api/controllers/tasks.js
+++ 3-Task-manager-api/controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require('../models/Task')
 const asyncWrapper = require('../middlewear/async')
+const { createCustomError } = require('../errors/custom-error')
 
 const getAllTasks = asyncWrapper( async (req,res) =>{
     const tasks = await Task.find({})
@@ -10,11 +11,12 @@ const createTask = asyncWrapper(async (req,res) =>{
     const task = await Task.create(req.body)
     res.status(201).json({ task })
 })
-const getTask = asyncWrapper(async (req,res) =>{
+const getTask = asyncWrapper(async (req,res,next) =>{
     const { id: taskID } = req.params
     const task = await Task.findOne({ _id: taskID })
     if(!task) {
-        return res.status(404).json({ msg: 'Task not found' })
+        return next(createCustomError(`No task with id ${taskID}`, 404))
+        // return res.status(404).json({ msg: 'Task not found' })
     }
     res.status(200).json({task})
 })
@@ -22,7 +24,7 @@ const deleteTask = asyncWrapper(async (req,res) =>{
     const { id: taskID } = req.params
     const task = await Task.findOneAndDelete({ _id: taskID })
     if(!task) {
-        return res.status(404).json({ msg: 'Task not found' })
+        return next(createCustomError(`No task with id ${taskID}`, 404))
     }
     res.status(200).json({ msg: 'Task deleted' })
 })
@@ -30,7 +32,7 @@ const updateTask = asyncWrapper(async (req,res) =>{
     const { id: taskID } = req.params
     const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {new :true, runValidators:true})
     if (!task) {
-      return res.status(404).json({ msg: "Task not found" });
+        return next(createCustomError(`No task with id ${taskID}`, 404));
     }
     // res.status(200).json({id: taskID, data: req.body})
     res.status(200).json({ task })
--- 3-Task-manager-api/middlewear/error-handler.js
+++ 3-Task-manager-api/middlewear/error-handler.js
@@ -1,5 +1,11 @@
+const { CustomAPIError } = require('../errors/custom-error')
+ 
 const errorHandlerMiddlewear = (err,req,res,next) => {
-    return res.status(500).json({ msg:`something ${err}` })
+    if(err instanceof CustomAPIError) {
+        res.status(err.statusCode).json({ msg: err.message })
+    } 
+    return res.status(500).json({ msg: 'Something went wrong!'})
+    
 }
 
 module.exports = errorHandlerMiddlewear
