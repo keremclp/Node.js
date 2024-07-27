11427c3819da8074be1d3d9fe451a8c85df315a7
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-07-27 16:34:06 +0300

    add: delete and update

--- 3-Task-manager-api/controllers/tasks.js
+++ 3-Task-manager-api/controllers/tasks.js
@@ -32,12 +32,31 @@ const getTask = async (req,res) =>{
     }
     
 }
-const updateTask = (req,res) =>{
-    res.send('update item')
-
+const deleteTask = async (req,res) =>{
+    try {
+        const { id: taskID } = req.params
+        const task = await Task.findOneAndDelete({ _id: taskID })
+        if(!task) {
+            return res.status(404).json({ msg: 'Task not found' })
+        }
+        res.status(200).json({ msg: 'Task deleted' })
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
-const deleteTask = (req,res) =>{
-    res.send('delete item')
+const updateTask = async (req,res) =>{
+    try {
+        const { id: taskID } = req.params
+        const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {new :true, runValidators:true})
+        if (!task) {
+          return res.status(404).json({ msg: "Task not found" });
+        }
+        // res.status(200).json({id: taskID, data: req.body})
+        res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error });
+        
+    }
 
 }
 
