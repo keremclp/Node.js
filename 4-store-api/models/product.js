e2eb7fe804edc66b94a60d42cfc64cb5b3813675
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-07-28 18:53:40 +0300

    add: query params

--- 4-store-api/models/product.js
+++ 4-store-api/models/product.js
@@ -9,7 +9,7 @@ const productSchema = new mongoose.Schema({
     type: Number,
     required: [true, "price is necessary"],
   },
-  feature: {
+  featured: {
     type: Boolean,
     default: false,
   },
