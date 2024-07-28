6182921be16b793d371dd17618e84765022365d0
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-07-28 13:31:40 +0300

    add: setup routes

--- 4-store-api/app.js
+++ 4-store-api/app.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
 
 const notFoundMiddleweare = require('./middleware/not-found')
 const errorMiddleWare = require('./middleware/error-handler')
@@ -18,6 +19,8 @@ app.use(express.json())
 app.get('/', (req,res)=>{
     res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
+
+app.use('/api/v1/products', productsRouter)
 
 // products route
 
