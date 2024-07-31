c8c1d288f676e538243c91814856372d0f19e95d
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-07-31 18:30:55 +0300

    create token and get with headers

--- 5-JWT-project/controllers/main.js
+++ 5-JWT-project/controllers/main.js
@@ -1,14 +1,21 @@
 const CustomAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken')
 const login = async (req, res) => {
     const { username, password } = req.body
     if(!username || !password){
         throw new CustomAPIError('Please provide username and password', 400)
     }
+    
+    const id = new Date().getDate()
+    
+    const token = jwt.sign({id,username}, process.env.JWT_SECRET, {expiresIn:'30d'})
+    res.status(201).json({msg: 'user created', token})
+
     console.log(username, password);
-    res.send("Fake login/register/signup Route");
 };
 
 const dashboard = async (req, res) => {
+  console.log(req.headers);
   const luckyNumber = Math.floor(Math.random() * 100);
   res
     .status(200)
