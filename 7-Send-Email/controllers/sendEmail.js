96bc257be34ef92952a41504a253897deb8ff7c9
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-08-05 21:05:07 +0300

    send email controllers

--- 7-Send-Email/controllers/sendEmail.js
+++ 7-Send-Email/controllers/sendEmail.js
@@ -1,5 +1,22 @@
+const nodemailer = require('nodemailer')
+
 const sendEmail = async (req,res) =>{
-    res.send('send email func');
+    let testAccount = await nodemailer.createTestAccount();
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      auth: {
+        user: "cassandra.schulist0@ethereal.email",
+        pass: "7NBTv4bkWSxUUgm1NE",
+      },
+    });
+    let info = await transporter.sendMail({
+        from: '"CLP Software" <kerem00962@gmail.com>',
+        to: 'bar@example.com',
+        subject:'Hello',
+        html:'<h2>Sending emails with Node.js</h2>'
+    })
+    res.json(info);
 }
 
 module.exports = sendEmail
